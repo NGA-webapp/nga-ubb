d463e8e8208d680189b76bdc9273f62dd992574b
yelo <zhihuzeye@gmail.com>
2013-11-07 17:55:09 +0800

    add grunt task for webapp repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function (grunt) {
+  var config = grunt.file.readJSON('config.json');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     mocha: {
@@ -9,12 +11,30 @@ module.exports = function (grunt) {
         inputDirectory: 'libs',
         outputDirectory: 'libs-cov'
       }
+    },
+    clean: {
+      webapp: {
+        src: [config.path.webapp],
+        options: {
+          force: true
+        }
+      }
+    },
+    copy: {
+      webapp: {
+        files: [
+          {extend: true, src: ['index.js', 'libs/**'], dest: config.path.webapp}
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks("grunt-jscoverage");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
+  grunt.registerTask('webapp', ['clean', 'copy']);
   grunt.registerTask('test', ['mocha']);
   grunt.registerTask('cov', ['jscoverage']);
 };
