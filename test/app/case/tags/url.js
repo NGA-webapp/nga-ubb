ab6a20b14fadd5ec102d7b0135c6952d0d55532e
yelo <zhihuzeye@gmail.com>
2013-11-26 15:04:32 +0800

    add url tag

---
+++ test/app/case/tags/url.js
@@ -0,0 +1,16 @@
+define(function (require, exports, module) {
+  return function (ubb) {
+    describe('url', function () {
+      var test = function (ubb, text, output) {
+        describe(text, function () {
+          it('should be ' + output, function () {
+            expect(ubb.toHtml(text)).to.be.equal(output);
+          });
+        });
+      };
+      text = '[url=foobar]baz[/url]';
+      output = '<a href="foobar">baz</a>';
+      test(ubb, text, output);
+    });
+  };
+});
