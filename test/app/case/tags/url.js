2ad38fcc0c633255362a22cf33f795be55e36607
yelo <zhihuzeye@gmail.com>
2013-11-27 15:21:07 +0800

    roll back

--- test/app/case/tags/url.js
+++ test/app/case/tags/url.js
@@ -9,10 +9,10 @@ define(function (require, exports, module) {
         });
       };
       text = '[url=foobar]baz[/url]';
-      output = '<a class="url" href="foobar" target="_system">baz</a>';
+      output = '<a class="url" data-url="foobar" href="javascript:void(0);">baz</a>';
       test(ubb, text, output);
       text = '[url]foobar[/url]';
-      output = '<a class="url" href="foobar" target="_system">foobar</a>';
+      output = '<a class="url" data-url="foobar" href="javascript:void(0);">foobar</a>';
       test(ubb, text, output);
     });
   };
