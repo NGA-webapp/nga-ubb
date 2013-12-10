72ebc84fe1314dcee4ae30536a0fb12c5a9d112b
yelo <zhihuzeye@gmail.com>
2013-12-10 10:03:28 +0800

    support node

--- libs/index.js
+++ libs/index.js
@@ -1,4 +1,18 @@
-define(function (require, exports) {
+;(function (definition) {
+  // this is considered "safe":
+  var hasDefine = typeof define === 'function',
+    // hasDefine = typeof define === 'function',
+    hasExports = typeof module !== 'undefined' && module.exports;
+  if (hasDefine) {
+    // AMD Module or CMD Module
+    define(definition);
+  } else if (hasExports) {
+    // Node.js Module
+    definition(require, exports, module);
+  } else {
+    throw new Error('module required');
+  }
+})(function (require, exports, module) {
   var Ubb = require('./Ubb').Ubb;
   var font = require('./tags/font');
   var layout = require('./tags/layout');
@@ -33,5 +47,5 @@ define(function (require, exports) {
   ubb.addExtra(brExtra.br);
   ubb.addExtra(smileExtra.smile);
   ubb.addExtra(encodeExtra.amp);
-  return ubb;
+  module.exports = ubb;
 });
